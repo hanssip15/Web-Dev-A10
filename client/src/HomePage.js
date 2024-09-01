cc71e522db851f42df45950c62fc7fe056d10655
hanssip15 <raihanmian1@gmail.com>
2024-09-01 23:13:06 +0700

    Update terkait Home Page

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -19,8 +19,31 @@ function HomePage() {
 
   return (
     <div>
+
+      <header>
+        <logo>Movie Review</logo>
+        <menu>Menu</menu>
+        <div class="search-container">
+            <input type="text" placeholder="Search Movie" class="search-input"/>
+            <button type="submit" class="search-button">Search</button>
+        </div>
+        <watchlist>Watchlist</watchlist>
+        <signin>Sign In</signin>
+        <language>EN</language>
+      </header>
+
+      <body>
+        <div class="image-container">
+          <img src="/img/film1.jpg" alt="Boboiboy Movie" class="styled-image"/>
+          <div class="gradient-overlay"></div>
+          <div class="next-film"></div>
+            <upnext>Up next</upnext>
+            <div class="square"></div> 
+        </div>
+ 
+      </body>
       <h1>Movie and TV Show Reviews</h1>
-      
+      <link rel="stylesheet" href="/css/style.css"></link>
       <h2>Movies</h2>
       <ul>
         {movies.map((movie, index) => (
