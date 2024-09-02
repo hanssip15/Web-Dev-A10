7fe74e06707ee92a27e6542d69608f482588f6f0
hanssip15 <raihanmian1@gmail.com>
2024-09-02 11:06:08 +0700

    HomePage Fix

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,25 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
 function HomePage() {
-  const [movies, setMovies] = useState([]);
-  const [tvShows, setTvShows] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const movieRes = await axios.get('/api/movies');
-      const tvShowRes = await axios.get('/api/tvshows');
-      setMovies(movieRes.data);
-      setTvShows(tvShowRes.data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-
+      <link rel="stylesheet" href="/css/style.css"></link>
       <header>
         <logo>Movie Review</logo>
         <menu>Menu</menu>
@@ -31,38 +13,55 @@ function HomePage() {
         <signin>Sign In</signin>
         <language>EN</language>
       </header>
-
       <body>
         <div class="image-container">
           <img src="/img/film1.jpg" alt="Boboiboy Movie" class="styled-image"/>
           <div class="gradient-overlay"></div>
-          <div class="next-film"></div>
-            <upnext>Up next</upnext>
-            <div class="square"></div> 
+          <div class="text-film">
+              <filmtitle>Lorem</filmtitle>
+              <subtitle>Watch Lorem Trailer</subtitle>
+          </div>
+          <div class="play-button-container">
+            <button class="play-button">
+              <span class="play-icon">▶</span>
+            </button>
+          </div>
         </div>
- 
+        <div class="next-film">
+          <upnext>Up next</upnext>
+          <div class="square">
+            <div class="item">
+            <img src="/img/film1.jpeg" alt="Boboiboy Mv" class="styled-image-preview"/>
+            <div class="play-button-container-mini">
+              <button class="play-button-mini">
+               <span class="play-icon-mini">▶</span>
+              </button>
+            </div>
+            <p>Lorem Ipsum Dolor</p>  
+            </div>  
+            <div class="item">
+            <img src="/img/film1.jpeg" alt="Boboiboy Mv" class="styled-image-preview"/>
+            <div class="play-button-container-mini">
+              <button class="play-button-mini">
+               <span class="play-icon-mini">▶</span>
+              </button>
+            </div>
+            <p>Lorem Ipsum Dolor</p>  
+            </div>
+            <div class="item">
+            <img src="/img/film1.jpeg" alt="Boboiboy Mv" class="styled-image-preview"/>
+            <div class="play-button-container-mini">
+              <button class="play-button-mini">
+               <span class="play-icon-mini">▶</span>
+              </button>
+            </div>
+            <p>Lorem Ipsum Dolor</p>
+            </div>
+          </div>
+          <browse>Browse trailers</browse>
+        </div>
       </body>
-      <h1>Movie and TV Show Reviews</h1>
-      <link rel="stylesheet" href="/css/style.css"></link>
-      <h2>Movies</h2>
-      <ul>
-        {movies.map((movie, index) => (
-          <li key={index}>
-            <Link to={`/movie/${encodeURIComponent(movie.name)}`}>{movie.name}</Link> - {movie.genre}
-          </li>
-        ))}
-      </ul>
-
-      <h2>TV Shows</h2>
-      <ul>
-        {tvShows.map((show, index) => (
-          <li key={index}>
-            <Link to={`/tvshow/${encodeURIComponent(show.name)}`}>{show.name}</Link> - {show.genre}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
-
 export default HomePage;
