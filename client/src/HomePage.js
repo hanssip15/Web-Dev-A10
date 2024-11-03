e6c87bff7b7293576d5379a3feb122e73a1e7e2f
F-Myers. <fmy150504@gmail.com>
2024-11-03 16:12:45 +0700

    Update Review, Search, Filter
    Secara Fungsionalitas Review, Search, Filter, Login, Register sudah berjalan lancar

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -2,212 +2,59 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import MHeader from './components/MHeader.js';
-  
+
 function HomePage() {
   const [movies, setMovies] = useState([]);
   const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
-<<<<<<< Updated upstream
-=======
-  const [searchQuery, setSearchQuery] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
-  const navigate = useNavigate();
->>>>>>> Stashed changes
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const movieRes = await axios.get('/api/movies');
-        setMovies(movieRes.data); // Data dari server (MongoDB)
+        console.log('Movies data in HomePage:', movieRes.data); // Tambahkan log ini
+        setMovies(movieRes.data);
       } catch (error) {
         console.error('Error fetching movies:', error);
       }
     };
-
-    fetchData();  
-  }, []);
-  useEffect(() => {
-    // Fetch movie data from your database here
-    // Replace this with your actual data fetching logic
-    const fetchMovies = async () => {
-      const response = await fetch('/your-api-endpoint');
-      const data = await response.json();
-      setMovies(data);
-    };
-    fetchMovies();
-  }, []);
+    fetchData();
+  }, []);  
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentMovieIndex((prevIndex) => (prevIndex + 1) % movies.length);
-    }, 5000); // Change the interval to 5000 for 5 seconds
+    }, 5000);
     return () => clearInterval(interval);
   }, [movies.length]);
-<<<<<<< Updated upstream
+
+  useEffect(() => {
+    console.log(movies); // Ini akan membantu melihat apakah data `country`, `genre`, dan `actor` benar-benar ada
+  }, [movies]);  
 
   return (
     <div>
-=======
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const movieRes = await axios.get('/api/movies');
-        setMovies(movieRes.data); // Data dari server (MongoDB)
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      }
-    };
-
-    fetchData();  
-  }, []);
-
-  useEffect(() => {
-    // Fetch movie data from your database here
-    // Replace this with your actual data fetching logic
-    const fetchMovies = async () => {
-      const response = await fetch('/your-api-endpoint');
-      const data = await response.json();
-      setMovies(data);
-    };
-    fetchMovies();
-  }, []);
-
-  const handleSignOut = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
-    setIsAdmin(false);
-    window.location.href = '/';
-  };
-
-  return (
-    <div>
-      <header>
-        <logo> <Link to="/">Movie Review</Link></logo>
-        <div className="dropdown">
-          <button className="btn btn-danger dropdown-toggle" type="button" id="menuDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-            Menu
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="menuDropdown">
-            <li><Link className="dropdown-item" to="/trending">Trending Movies</Link></li>
-            <li><Link className="dropdown-item" to="/new-releases">New Releases</Link></li>
-            {isAdmin && (
-              <li><Link className="dropdown-item" to="/admin/movie-requests">Manage Movie Requests</Link></li>
-            )}
-            {isLoggedIn && (
-              <li><Link className="dropdown-item" to="/request-movie">Request a Movie</Link></li>
-            )}
-          </ul>
-        </div>
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Search Movie"
-            className="search-input"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button onClick={handleSearch} className="search-button">Search</button>
-        </div>
-        <watchlist>Watchlist</watchlist>
-        <div className="signin-container">
-        {isLoggedIn ? (
-          <button className="signout-button" onClick={handleSignOut}>Sign Out</button>
-        ) : (
-          <Link to="/login" className="signin-link">Sign In</Link>
+      <MHeader />
+      <div className="image-container">
+        {movies.length > 0 && (
+          <>
+            <img
+              src={movies[currentMovieIndex].image}
+              alt={movies[currentMovieIndex].title}
+              className="styled-image"
+            />
+            <div className="movie-details">
+              <h2>{movies[currentMovieIndex].title}</h2>
+              <p>{movies[currentMovieIndex].synopsis}</p>
+              <p>Country: {movies[currentMovieIndex].country ? movies[currentMovieIndex].country.name : 'Unknown'}</p>
+              <p>Genre: {movies[currentMovieIndex].genre && movies[currentMovieIndex].genre.length > 0 ? movies[currentMovieIndex].genre.map(g => g.name).join(', ') : 'N/A'}</p>
+              <p>Actors: {movies[currentMovieIndex].actor && movies[currentMovieIndex].actor.length > 0 ? movies[currentMovieIndex].actor.map(a => a.name).join(', ') : 'Unknown'}</p>
+              <Link to={`/movie/${encodeURIComponent(movies[currentMovieIndex].title)}`} className="moreButton">More Details</Link>
+            </div>
+          </>
         )}
-        </div>
-        <language>EN</language>
-      </header>
-
-      <body>
-        <div className="image-container">
-            {movies.length > 0 && (
-              <>
-                <img 
-                  src={`/img/poster/${movies[currentMovieIndex].image}.jpg`} 
-                  alt={movies[currentMovieIndex].title} 
-                  className="styled-image" 
-                />
-                <div className="movie-details">
-                  <h2>{movies[currentMovieIndex].title}</h2> {/* Nama film */}
-                  <p>{movies[currentMovieIndex].synopsis}</p> {/* Sinopsis film */}
-                  <Link to={`/movie/${encodeURIComponent(movies[currentMovieIndex].title)}`} className='moreButton'> More Contents
-                  </Link>
-                </div>
-              </>
-            )}
-        </div>
-        <div class="next-film">
-            <upnext>Up next</upnext>
-            <ul>
-              {movies.map((movie, index) => (
-              <li key={index}>
-                <div class="item">
-                <img src={`/img/poster/${movie.image}.jpg`} alt={movie.title} className="styled-image-preview"/>
-                <Link to={`/movie/${encodeURIComponent(movie.title)}`}>
-                  <div class="play-button-container-mini">
-                  <div class="play-mtitle">{movie.title}
-                    <button class="play-button-mini">
-                      <span class="play-icon-mini">▶</span>
-                    </button>
-                    </div>
-                  </div>
-                </Link>
-                </div>
-              </li>
-            ))}
-            </ul>
-            <browse>Browse trailers </browse>
-        </div>
-      </body>
-      
->>>>>>> Stashed changes
-      <link rel="stylesheet" href="/css/style.css"></link>
-      <MHeader/>
-        <body>
-        <div className="image-container">
-            {movies.length > 0 && (
-              <>
-                <img 
-                  src={`/img/poster/${movies[currentMovieIndex].image}.jpg`} 
-                  alt={movies[currentMovieIndex].title} 
-                  className="styled-image" 
-                />
-                <div className="movie-details">
-                  <h2>{movies[currentMovieIndex].title}</h2> {/* Nama film */}
-                  <p>{movies[currentMovieIndex].synopsis}</p> {/* Sinopsis film */}
-                  <Link to={`/movie/${encodeURIComponent(movies[currentMovieIndex].title)}`} className='moreButton'> More Contents
-                  </Link>
-                </div>
-              </>
-            )}
-        </div>
-        <div class="next-film">
-            <upnext>Up next</upnext>
-            <ul>
-              {movies.map((movie, index) => (
-              <li key={index}>
-                <div class="item">
-                <img src={`/img/poster/${movie.image}.jpg`} alt={movie.title} className="styled-image-preview"/>
-                <Link to={`/movie/${encodeURIComponent(movie.title)}`}>
-                  <div class="play-button-container-mini">
-                  <div class="play-mtitle">{movie.title}
-                    <button class="play-button-mini">
-                      <span class="play-icon-mini">▶</span>
-                    </button>
-                    </div>
-                  </div>
-                </Link>
-                </div>
-              </li>
-            ))}
-            </ul>
-            <browse>Browse trailers </browse>
-        </div>
-      </body>
-
+      </div>
     </div>
   );
 }
+
 export default HomePage;
--- client/src/MovieDetailPage.js
+++ client/src/MovieDetailPage.js
@@ -1,70 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import {jwtDecode} from 'jwt-decode'; // Tidak perlu destructuring
 import './MovieDetailPage.css';
 import MHeader from './components/MHeader.js';
 
 function MovieDetailPage() {
   const { title } = useParams();
   const [movie, setMovie] = useState(null);
-  const [rating, setRating] = useState('');
+  const [rating, setRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);  
   const [comment, setComment] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
-  const [hoverRating, setHoverRating] = useState(0);
-  const totalStars = 5; // Jumlah bintang
-
-  const handleRatingClick = (index) => {
-    setRating(index);
-  };
 
   useEffect(() => {
     const fetchMovie = async () => {
       try {
         const res = await axios.get(`/api/movies/${encodeURIComponent(title)}`);
-        setMovie(res.data); // Pastikan movie diambil dari response
+        setMovie(res.data);
       } catch (error) {
         setErrorMessage('Failed to load movie details or reviews.');
       }
     };
 
-<<<<<<< Updated upstream
-    fetchMovieAndReviews();
-=======
     fetchMovie();
-
-    // Cek status login dari token
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsLoggedIn(true);
-    }
->>>>>>> Stashed changes
   }, [title]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (rating === '') {
+  
+    if (rating === 0) {
       setErrorMessage('Rating is required.');
       return;
     }
-
+  
     try {
       const token = localStorage.getItem('token');
       if (!token) {
         setErrorMessage('You must be logged in to submit a review.');
         return;
       }
-      const decodedToken = jwtDecode(token);
-
+  
       await axios.post(
         '/api/movies/rate',
         {
           title: movie.title,
           rating,
-          reviewText: comment, // Gunakan 'reviewText' untuk mengirim komentar
-          name: decodedToken.name,
+          reviewText: comment
         },
         {
           headers: {
@@ -72,18 +54,30 @@ function MovieDetailPage() {
           },
         }
       );
-      setRating('');
+      setRating(0);
       setComment('');
       setErrorMessage('');
       setSuccessMessage('Review submitted successfully!');
-
+  
       // Fetch ulang data movie untuk mendapatkan review terbaru
-      const res = await axios.get(`/api/movies/${encodeURIComponent(title)}`);
+      const res = await axios.get(`/api/movies/${encodeURIComponent(movie.title)}`);
       setMovie(res.data); // Update movie dengan data terbaru
-
     } catch (error) {
       setErrorMessage('Failed to submit review. Please try again.');
+      console.error("Error submitting review:", error);
     }
+  };
+  
+  const handleRatingClick = (index) => {
+    setRating(index);
+  };
+  
+  const handleMouseEnter = (index) => {
+    setHoverRating(index);
+  };
+  
+  const handleMouseLeave = () => {
+    setHoverRating(0);
   };
 
   if (!movie) return <p>Loading...</p>;
@@ -91,105 +85,56 @@ function MovieDetailPage() {
   return (
     <div>
       <link rel="stylesheet" href="/css/style.css"></link>
-      <MHeader/>
-      <body>
+      <MHeader />
       <div className="movie-detail-container">
-          <div className="image-container">
-          <img
-            src={`/img/poster/${movie.image}.jpg`}
-            alt={movie.title}
-            className="styled-image"
-          />
+        <div className="image-container">
+        <img
+          src={movie.image}
+          alt={movie.title}
+          className="styled-image"
+        />
           <div className='content-container'>
-            <h1 className= "custom-h1">{movie.title}</h1>
+            <h1 className="custom-h1">{movie.title}</h1>
             <p>
-            <h3 className='custom-h3'> Synopsis: {movie.synopsis} </h3>
+              <strong>Synopsis:</strong> {movie.synopsis}
             </p>
             <p>
-            <h3 className='custom-h3'> Country: {movie.country}</h3>
+              <strong>Country:</strong> {movie.country ? movie.country.name : 'Unknown'}
             </p>
             <p>
-            <h3 className='custom-h3'> Genre: {movie.genre.join(', ')} </h3>
+              <strong>Genre:</strong> {movie.genre ? movie.genre.map(g => g.name).join(', ') : 'N/A'}
             </p>
             <p>
-            <h3 className='custom-h3'> Actor: {movie.actor ? movie.actor.join(', ') : 'Unknown'} </h3>
+              <strong>Actor:</strong> {movie.actor ? movie.actor.map(a => a.name).join(', ') : 'Unknown'}
             </p>
             <p>
-            <h3 className='custom-h3'> Release Year: {movie.releaseYear} </h3>
+              <strong>Release Year:</strong> {movie.releaseYear}
+            </p>
+            <p>
+              <strong>Average Rating:</strong> {movie.averageRating ? movie.averageRating.toFixed(1) : 'N/A'}
             </p>
           </div>
-          </div>
+        </div>
       </div>
-      <div className='review-detail-container'>
-      <h3>Submit Your Review</h3>
-      <form onSubmit={handleSubmit} className="review-form">
+
+      <div className="review-detail-container">
+        <h3>Submit Your Review</h3>
+        <form onSubmit={handleSubmit} className="review-form">
         <div className="star-rating">
-          {[...Array(totalStars)].map((_, index) => {
+          {[...Array(5)].map((_, index) => {
             return (
               <span
                 key={index}
                 className={`star ${index < (hoverRating || rating) ? 'filled' : ''}`}
                 onClick={() => handleRatingClick(index + 1)}
-                onMouseEnter={() => setHoverRating(index + 1)}
-                onMouseLeave={() => setHoverRating(0)}
+                onMouseEnter={() => handleMouseEnter(index + 1)}
+                onMouseLeave={handleMouseLeave}
               >
                 &#9733; {/* Unicode untuk simbol bintang */}
               </span>
             );
           })}
         </div>
-        <input
-          type="text"
-          placeholder="Comment"
-          value={reviewText}
-          onChange={(e) => setReviewText(e.target.value)}
-          className="review-input"
-        />
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
-
-<<<<<<< Updated upstream
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
-
-      <div className="review-container">
-        <h3>Reviews</h3>
-        {reviews.length > 0 ? (
-          reviews.map((review, index) => (
-            <div key={index} className="review-item">
-              <p>
-                <strong>{review.userId.name}</strong>
-              </p>
-              <p>
-                <div className="star-rating">
-                  {[...Array(totalStars)].map((_, i) => (
-                    <span key={i} className={`star ${i < review.rating ? 'filled' : ''}`}>
-                      &#9733;
-                    </span>
-                  ))}
-                </div>
-              </p>
-              <p>Comment: {review.reviewText}</p>
-              <p>Date: {new Date(review.reviewDate).toLocaleDateString()}</p>
-            </div>
-          ))
-        ) : (
-          <p>No reviews yet.</p>
-        )}
-=======
-        <h3>Submit Your Review</h3>
-        <form onSubmit={handleSubmit} className="review-form">
-          <input
-            type="number"
-            placeholder="Rating"
-            value={rating}
-            onChange={(e) => setRating(e.target.value)}
-            className="review-input"
-            min="1"
-            max="5"
-          />
           <input
             type="text"
             placeholder="Comment"
@@ -197,37 +142,30 @@ function MovieDetailPage() {
             onChange={(e) => setComment(e.target.value)}
             className="review-input"
           />
-          <button type="submit" className="submit-button">
-            Submit
-          </button>
+          <button type="submit" className="submit-button">Submit</button>
         </form>
 
         {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
 
         <div className="review-container">
-          {/* Bagian untuk menampilkan reviews */}
           <h3>Reviews</h3>
           {movie.reviews && movie.reviews.length > 0 ? (
             movie.reviews.map((review, index) => (
               <div key={index} className="review-item">
                 <p>
-                  Name: <strong>{review.name || 'Anonymous'}</strong>
+                  Name: <strong>{review.userId.name || 'Anonymous'}</strong>
                 </p>
                 <p>Rate: {review.rating}/5</p>
-                <p>Comment: {review.comment}</p>
+                <p>Comment: {review.reviewText}</p>
               </div>
             ))
           ) : (
             <p>No reviews yet.</p>
           )}
         </div>
->>>>>>> Stashed changes
       </div>
     </div>
-      </body>
-    </div>
-    
   );
 }
 
